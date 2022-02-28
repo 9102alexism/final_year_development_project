662d239dbd2d55392db797420f2bd66eba264d93
Fahim Shakil <fshakil163014@bscse.uiu.ac.bd>
2022-02-28 14:21:13 +0600

    nothing_much

--- front_end/pharmacy/js/home.js
+++ front_end/pharmacy/js/home.js
@@ -8,6 +8,31 @@ let total_count = 0
 let history = []
 var tbody = document.getElementsByTagName("tbody")[0]
 
+var labels = []
+var values = []
+
+var ctx = document.getElementById("chart_1").getContext("2d")
+
+var data = {
+    labels,
+    datasets: [
+        {
+            data: values,
+            label: "Monthly Sale",
+            backgroundColor: "#fff",
+            borderColor: "#008a8a"
+        }
+    ]
+}
+var config = {
+    type: "line",
+    data: data,
+    option: {
+        responsive: true
+    }
+}
+var chart = new Chart(ctx, config)
+
 var xhr = new XMLHttpRequest()
 xhr.responseType = "json"
 let url = "http://3d94-43-224-111-192.ngrok.io"
@@ -19,6 +44,42 @@ xhr.onload = () => {
             total_count += dt["total"]
         }
         document.getElementById("amount").innerHTML = total_count.toFixed(2)
+
+        xhr.onload = () => {
+            if(xhr.status == 200 && xhr.readyState == 4){
+                let resp = xhr.response
+                labels = Object.keys(xhr.response)
+                for(let i of labels){
+                    values.push(resp[i])
+                }
+                data = {
+                    labels,
+                    datasets: [
+                        {
+                            data: values,
+                            label: "Monthly Sale",
+                            backgroundColor: "#fff",
+                            borderColor: "#008a8a"
+                        }
+                    ]
+                }
+                config = {
+                    type: "line",
+                    data: data,
+                    option: {
+                        responsive: true
+                    }
+                }
+                chart.destroy()
+                chart = new Chart(ctx, config)
+            }
+            else{
+                console.log("Something Is Wrong")
+            }
+        }
+        xhr.open("GET", url + "/pharmacies/saleSummary")
+        xhr.setRequestHeader("Authorization", user_data["token"])
+        xhr.send()
     }
     else{
         console.log("Something Is Wrong")
@@ -27,56 +88,6 @@ xhr.onload = () => {
 xhr.open("POST", url + "/pharmacies/invoices")
 xhr.setRequestHeader("Authorization", user_data["token"])
 xhr.send()
-
-const ctx_1 = document.getElementById("chart_1").getContext("2d")
-const ctx_2 = document.getElementById("chart_2").getContext("2d")
-
-const labels = [
-    "2016",
-    "2017",
-    "2018",
-    "2019",
-    "2020"
-]
-const data_1 = {
-    labels,
-    datasets: [
-        {
-            data: [211, 326, 165, 350, 420],
-            label: "Monthly Sale",
-            backgroundColor: "#fff",
-            borderColor: "#008a8a"
-        }
-    ]
-}
-const config_1 = {
-    type: "line",
-    data: data_1,
-    option: {
-        responsive: true
-    }
-}
-const chart_1 = new Chart(ctx_1, config_1)
-
-const data_2 = {
-    labels,
-    datasets: [
-        {
-            data: [211, 326, 165, 350, 420],
-            label: "Top 5 Product",
-            backgroundColor: ["green"],
-            borderColor: "#008a8a"
-        }
-    ]
-}
-const config_2 = {
-    type: "bar",
-    data: data_2,
-    option: {
-        responsive: true
-    }
-}
-const chart_2 = new Chart(ctx_2, config_2)
 
 function add_row(_data_available){
     var row = tbody.insertRow()
